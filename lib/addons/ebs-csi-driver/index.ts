9f706770df45b7d8edd1a8802832f7316f8c2354
chansuke <moonset20@gmail.com>
2025-07-17 22:39:18 +0900

    fix(ebs-csi-driver): add the compatibility for Kubernetes v1.33

--- lib/addons/ebs-csi-driver/index.ts
+++ lib/addons/ebs-csi-driver/index.ts
@@ -10,6 +10,7 @@ import { KubernetesManifest, KubernetesPatch } from "aws-cdk-lib/aws-eks";
 
 /* VersioMap showing the default version for 4 supported Kubernetes versions */
 const versionMap: Map<KubernetesVersion, string> = new Map([
+  [KubernetesVersion.V1_33, "v1.42.0-eksbuild.1"],
   [KubernetesVersion.V1_31, "v1.42.0-eksbuild.1"],
   [KubernetesVersion.V1_30, "v1.36.0-eksbuild.1"],
   [KubernetesVersion.V1_29, "v1.36.0-eksbuild.1"],
